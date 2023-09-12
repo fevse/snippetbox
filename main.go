a53854f0ae0edb4a9d1e9939138ce7c144f8a9a4
fevse <fedulov.v.s@gmail.com>
2023-09-12 20:21:53 +0600

    add showSnippet and createSnippet handlers

--- main.go
+++ main.go
@@ -9,11 +9,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Showing a note"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Form for creating a new note"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Starting web-server http://127.0.0.1.4000")
+	log.Println("Running a web-server on http://127.0.0.1.4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
