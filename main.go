a555935aecd179d610928a1b8fa2a3f8b4959c5d
fevse <fedulov.v.s@gmail.com>
2023-09-14 22:37:20 +0600

    update showSnippet handler

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Showing a note"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Showing a note #%d", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
