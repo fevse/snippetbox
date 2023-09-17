376c41f93a1c4ca4fd32b4fa0097e5c836142386
fevse <fedulov.v.s@gmail.com>
2023-09-17 19:13:37 +0600

    update home handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
-	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +14,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
