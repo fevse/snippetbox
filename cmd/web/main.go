5cba386af2bc7edf39e8e5c67dd6b419dcdbc349
fevse <fedulov.v.s@gmail.com>
2023-09-20 22:39:01 +0600

    add Open meth for fileServer

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 
@@ -13,7 +14,35 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(neuteredFileSistem{http.Dir("./ui/static")})
+
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
 	log.Println("Running a web-server on http://127.0.0.1.4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSistem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSistem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+	return f, nil
 }
