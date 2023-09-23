fddd892cdc9df14bc6c2e64824e96d9c2c4c542c
fevse <fedulov.v.s@gmail.com>
2023-09-23 21:31:08 +0600

    dependency injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,7 +8,10 @@ import (
 	"path/filepath"
 )
 
-
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
 
 func main() {
 	addr := flag.String("addr", ":4000", "Net adress HTTP")
@@ -17,10 +20,15 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(neuteredFileSistem{http.Dir("./ui/static")})
 
