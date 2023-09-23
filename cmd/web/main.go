9a17ac9c188330c3822939e7fa0edc57dfe3ff26
fevse <fedulov.v.s@gmail.com>
2023-09-23 22:01:35 +0600

    add routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -25,19 +25,10 @@ func main() {
 		infoLog: infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(neuteredFileSistem{http.Dir("./ui/static")})
-
-	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.routes(),
 	}
 
 	infoLog.Printf("Running a web-server on %s", *addr)
