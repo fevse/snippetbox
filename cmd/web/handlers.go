2e23d7fa56280acab016877530eb81d7b3fbc5e2
fevse <fedulov.v.s@gmail.com>
2023-10-06 18:20:44 +0400

    update Get method

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"fevse/snippetbox/pkg/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -37,7 +40,18 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		app.NotFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Showing a note #%d", id)
+	
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.NotFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%v", s)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
